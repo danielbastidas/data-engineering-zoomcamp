a64797124a1bd5b84eee3d6c16b1761d64432042
dabgpsn@gmail.com <dabgpsn@gmail.com>
2026-06-11 08:57:25 +0000

    dbt module completed

---
+++ models/marts/fct_trips.sql
@@ -0,0 +1,8 @@
+/*
+To Do:
+- One row per trip (doesn't matter if yellow or green)
+- Add a primary key (trip_id). It has to be unique
+- Find all duplicates. Understand why they happen, and fix them.
+- Find a way to enrich the column payment type.
+*/
+select 1
